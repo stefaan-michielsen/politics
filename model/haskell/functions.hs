048b0ebbfd9f570d32821c1854eb35a98005473a
Stefaan Michielsen <stefaan.michielsen@gmail.be>
2016-07-28 23:32:41 +0200

    Add functions

--- model/haskell/functions.hs
+++ model/haskell/functions.hs
@@ -1,3 +1,13 @@
 doubleUs x y = x*2 + y*2
 doubleMe x = x*2
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+lucky :: (Integral a) => a -> String  
+lucky 7 = "LUCKY NUMBER SEVEN!"  
+lucky x = "Sorry, you're out of luck, pal!"   
+
+
+charName :: Char -> String  
+charName 'a' = "Albert"  
+charName 'b' = "Broseph"  
+charName 'c' = "Cecil"  
